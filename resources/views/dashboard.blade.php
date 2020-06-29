66810db2c1f807034485c0132973688f3369e9b4
muhammad refda <muhammadrefda@gmail.com>
2020-06-29 13:39:30 +0700

    add dashboard.blade.php

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('master')
+@section('content')
+    <div class="card">
+        <div class="card-header">
+
+        </div>
+        <div class="card-body">
+
+            <h5 class="card-title" align="center">Selamat datang di Sistem Input Soal SMP AMALIYAH JAKARTA</h5>
+            <p>
+{{--                Untuk menginput data soal silahkan pilih bank soal--}}
+            </p>
+            <p>
+{{--                Untuk melihat nilai siswa silahkan pilih nilai--}}
+            </p>
+            </div>
+    </div>
+@endsection
